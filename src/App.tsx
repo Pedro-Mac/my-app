b3c8addffdac297549f4a04cb30067c913e58604
KozaX <55134615+KozaX@users.noreply.github.com>
2020-12-31 14:00:04 +0100

    add header and projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
 import React from "react";
+import Project from "./components/Projects";
 
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <header>
+        <h1>HI! I AM PEDRO</h1>
+        <p>
+          I am a frontend developer and I am always looking for adventures,
+          wilderness, inspirational stories and for the best waves to surf.
+        </p>
+      </header>
+      <main>
+        <article>
+          <h2>Nice to meet you 😄</h2>
+          <p>
+            I have been building my skills around Javascript, focusing mainly in
+            React, Redux, Typescript, Bootstrap and Sass.
+          </p>
+        </article>
+
+        <section className="d-flex flex-wrap">
+          <Project />
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export default App;
