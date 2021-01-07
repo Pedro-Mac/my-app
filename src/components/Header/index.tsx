5ca2d5cd1338cd389b334f0d666848103688c35b
KozaX <55134615+KozaX@users.noreply.github.com>
2021-01-07 15:39:41 +0100

    add effect to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./header.scss";
 
 const Header = () => {
+  const [effect, setEffect] = useState("");
+  useEffect(() => {
+    setEffect("active-effect");
+  }, [effect]);
+
   return (
     <header className="d-flex flex-column justify-content-center intro-header wrapper">
-      <h1 className="text-center mb-5 primary-title">HI! I AM PEDRO</h1>
-      <p className="description-highlight">
+      <h1 className={`text-center mb-5 primary-title ${effect}`}>
+        HI! I AM PEDRO
+      </h1>
+      <p className={`description-highlight ${effect}`}>
         I am a frontend developer and I am always looking for adventures,
         wilderness, inspirational stories and for the best waves to surf.
       </p>
